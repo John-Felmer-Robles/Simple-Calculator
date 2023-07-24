dc67c131d06e857c4a1273351dc40e1bd192d836
John-Felmer-Robles <robles.johnfelmer2005@gmail.com>
2023-07-24 20:05:28 +0800

    Added Code.

---
+++ Code/Csharp Sandbox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float P, r, n;
+            double A;
+
+            Console.WriteLine("Investment Amount: ");
+            P = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Rate of Investment(per year): ");
+            r = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Duration of Fixed Deposit: ");
+            n = float.Parse(Console.ReadLine());
+
+            double x = Math.Pow(1 + (r / 100), n);
+
+            A = P * x;
+
+            Console.WriteLine(A);
+
+            Console.ReadLine();
+        }
+
+    }
+}
